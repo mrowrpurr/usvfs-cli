49892158e43b0235c111e54d23a6de879f83f25b
Mrowr Purr <mrowr.purr@gmail.com>
2023-08-20 20:30:52 -0700

    Verify working directory OK
    And use application name instead of as args.
    Unsure of difference / benefits of each (yet).

--- usvfs-cli.cpp
+++ usvfs-cli.cpp
@@ -10,27 +10,37 @@
 #include <codecvt>
 #include <iostream>
 #include <locale>
+#include <string>
 
-// TODO: get it working with different (non-deprecated) functions
-auto GetHappyPath(const std::string& path) {
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    return converter.from_bytes(path);
+// Le sigh.
+std::wstring GetHappyPath(const std::string& path) {
+    int wideSize = MultiByteToWideChar(CP_UTF8, 0, path.c_str(), -1, NULL, 0);
+    if (wideSize == ERROR_NO_UNICODE_TRANSLATION)
+        throw std::runtime_error("Invalid UTF-8 sequence.");
+    if (wideSize == 0) throw std::runtime_error("Error in conversion.");
+
+    std::wstring wideString(wideSize, L'\0');
+    if (MultiByteToWideChar(CP_UTF8, 0, path.c_str(), -1, &wideString[0], wideSize) == 0)
+        throw std::runtime_error("Error in conversion.");
+
+    return wideString;
 }
 
-bool RunProgram(const std::string& program, const std::string& workingDirectory) {
+bool RunProgram(const std::string& program, const std::wstring& workingDirectory) {
     STARTUPINFOW si = {};
     si.cb           = sizeof(si);
 
     PROCESS_INFORMATION pi      = {};
     BOOL                success = FALSE;
 
-    auto lpCommandLine      = std::wstring{GetHappyPath(program)};
-    auto lpWorkingDirectory = std::wstring{GetHappyPath(workingDirectory)};
+    auto lpApplicationName = std::wstring{GetHappyPath(program)};
+    // auto lpCommandLine      = std::wstring{GetHappyPath(program)};
+    auto lpWorkingDirectory = workingDirectory;
 
     std::cout << "Creating process..." << std::endl;
 
     auto result = CreateProcessHooked(
-        nullptr, &lpCommandLine[0], nullptr, nullptr, FALSE, 0, nullptr, lpWorkingDirectory.c_str(),
+        lpApplicationName.c_str(), NULL, NULL, NULL, FALSE, 0, NULL, lpWorkingDirectory.c_str(),
         &si, &pi
     );
 
@@ -57,7 +67,7 @@ int main() {
 
     usvfsCreateVFS(params);
 
-    auto workingDirectory = "C:/Code/mrowrpurr/usvfs-cli/fixtures";
+    auto workingDirectory = L"C:/Code/mrowrpurr";
 
     auto sourceAFolderPath =
         GetHappyPath("C:/Code/mrowrpurr/usvfs-cli/fixtures/source_folder/SourceA");
@@ -73,9 +83,9 @@ int main() {
     VirtualLinkDirectoryStatic(
         sourceAFolderPath.c_str(), destFolderPath.c_str(), LINKFLAG_RECURSIVE
     );
-    VirtualLinkDirectoryStatic(
-        sourceBFolderPath.c_str(), destFolderPath.c_str(), LINKFLAG_RECURSIVE
-    );
+    // VirtualLinkDirectoryStatic(
+    //     sourceBFolderPath.c_str(), destFolderPath.c_str(), LINKFLAG_RECURSIVE
+    // );
     VirtualLinkDirectoryStatic(
         sourceCFolderPath.c_str(), destFolderPath.c_str(), LINKFLAG_RECURSIVE
     );
@@ -84,7 +94,8 @@ int main() {
     );
 
     std::cout << "Running program..." << std::endl;
-    if (RunProgram("C:/Program Files/Notepad++/notepad++.exe", workingDirectory)) {
+    // if (RunProgram("C:/Program Files/Notepad++/notepad++.exe", workingDirectory)) {
+    if (RunProgram("c:/Program Files/Kate/bin/kate.exe", workingDirectory)) {
         std::cout << "Program finished" << std::endl;
     } else {
         std::cout << "Failed to run program" << std::endl;
