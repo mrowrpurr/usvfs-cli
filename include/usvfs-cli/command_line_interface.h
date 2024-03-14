d140e47fce75592657bce0b1693df2cd6e68d986
Mrowr Purr <mrowr.purr@gmail.com>
2024-03-14 01:45:01 -0700

    Can add multiple --process via CLI

--- include/usvfs-cli/command_line_interface.h
+++ include/usvfs-cli/command_line_interface.h
@@ -1,5 +1,7 @@
-#include <argparse/argparse.hpp>
+#include <argumentum/argparse.h>
+
 #include <nlohmann/json.hpp>
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -42,46 +44,26 @@ void from_json(const nlohmann::json& j, VirtualLink& d) {
 
 struct CommandOptions {
     WebSocketServerOptions webSocketServerOptions;
-    vector<LaunchProcess>  processes;
-    vector<VirtualLink>    virtualLinks;
+    vector<LaunchProcess>  processes{};
+    vector<VirtualLink>    virtualLinks{};
 };
 
-inline CommandOptions ParseOptions(int argc, char* argv[]) {
-    CommandOptions           options;
-    argparse::ArgumentParser parser("usvfs-cli");
+inline optional<CommandOptions> ParseOptions(int argc, char* argv[]) {
+    vector<string> processesJson;
 
-    // usvfs.exe \
-    //    --process '{"path":"C:/path/to/program.exe"}' \
-    //    --link-on-create '{"source":"C:/actual/path","overwrite":"C:/overwrite-folder"}' \
-    //    --link-dir '{"source":"C:/actual/path","target":"C:/virtual/path"}' \
-    //    --link-file '{"source":"C:/actual/file.txt","target":"C:/virtual/file.txt"}'
+    argumentum::argument_parser parser;
+    auto                        params = parser.params();
 
-    parser.add_argument("--run-server")
-        .help("Run the WebSocket server")
-        .default_value(false)
-        .implicit_value(true);
+    parser.config().program("usvfs-cli").description("usvfs command line interface");
 
-    parser.add_argument("--port")
-        .help("Port for the WebSocket server")
-        .default_value(8080)
-        .action([](const std::string& value) { return std::stoi(value); });
+    params.add_parameter(processesJson, "--process", "-p").minargs(1).help("Processes to launch");
 
-    parser.add_argument("--server-address")
-        .help("Address for the WebSocket server")
-        .default_value("localhost");
+    if (!parser.parse_args(argc, argv, 1)) return nullopt;
 
-    parser.add_argument("--process")
-        .help("Process to launch")
-        .nargs(1)
-        .action([&options](const std::string& value) {
-            options.processes.push_back(nlohmann::json::parse(value));
-        });
+    CommandOptions options;
 
-    try {
-        parser.parse_args(argc, argv);
-    } catch (const std::exception& err) {
-        std::cerr << err.what() << std::endl;
-        std::cerr << parser;
+    for (auto& processJson : processesJson) {
+        options.processes.push_back(nlohmann::json::parse(processJson));
     }
 
     return options;
--- main.cpp
+++ main.cpp
@@ -5,8 +5,25 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    auto options = ParseOptions(argc, argv);
+    // vector<string> fakeArguments = {
+    //     "program.exe", "--process",
+    //     "{\"path\":\"C:\\\\Program Files\\\\Notepad++\\\\notepad++.exe\"}"
+    // };
+    // int    fakeArgc = fakeArguments.size();
+    // char** fakeArgv = new char*[fakeArgc];
+    // for (int i = 0; i < fakeArgc; i++) {
+    //     fakeArgv[i] = const_cast<char*>(fakeArguments[i].c_str());
+    // }
+    // auto parsedOptions = ParseOptions(fakeArgc, fakeArgv);
 
+    auto parsedOptions = ParseOptions(argc, argv);
+
+    if (!parsedOptions) {
+        cout << "Failed to parse options" << endl;
+        return 1;
+    }
+
+    auto  options       = *parsedOptions;
     auto& serverOptions = options.webSocketServerOptions;
     // ..
 
